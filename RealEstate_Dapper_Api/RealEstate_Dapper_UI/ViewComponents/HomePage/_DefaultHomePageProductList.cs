a7f0ce49c33a451fbf10cd7f2331fffe960b6b19
Burak <onurburak149@gmail.com>
2023-09-29 23:25:49 +0300

    PartialTamamlanması
    PartialTamamlanması

--- RealEstate_Dapper_Api/RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultHomePageProductList.cs
+++ RealEstate_Dapper_Api/RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultHomePageProductList.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.ProductDto;
 
 namespace RealEstate_Dapper_UI.ViewComponents.HomePage
 {
-    public class _DefaultHomePageProductList : ViewComponent
-    {
-        public IViewComponentResult Invoke()
-        {
-            return View();
-        }
-    }
+	public class _DefaultHomePageProductList : ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _DefaultHomePageProductList(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:44347/api/Products/ProductListWithCategory");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+	}
 }
